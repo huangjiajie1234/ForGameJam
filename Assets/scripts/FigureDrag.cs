88681220294dbef9aa82151e1bbccb6bd4974228
malongtao <malongtao@xindong.com>
2017-12-08 16:29:47 +0800

    fig

---
+++ Assets/scripts/FigureDrag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FigureDrag : MonoBehaviour
+{
+    private float distOffset = 0;
+    private float curTouchDist = 0;
+    private float lastTouchDist = 0;
+    void Start()
+    {
+        Input.multiTouchEnabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount <= 0)
+        {
+            return;
+        }
+
+        if (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved)
+        {
+            var touch1 = Input.GetTouch(0);
+            var touch2 = Input.GetTouch(1);
+
+            curTouchDist = (touch1.position - touch2.position).magnitude;
+
+            distOffset = curTouchDist - lastTouchDist;
+
+            if (curTouchDist != lastTouchDist)
+            {
+                lastTouchDist = curTouchDist;
+            }
+
+			//this.gameObject.transform.localScale += Vector3.one * distOffset * Time.deltaTime ;
+        }
+    }
+}
